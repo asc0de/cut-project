14210d8974924c6be6e7c369c8b4a36219c8ad48
Vladislav <vladdex@mail.ru>
2018-02-03 19:52:11 +0300

    1) support of ssl certificate 2) automatic redirect to https from http

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
-var express = require('express')
-var app = express()
+const http = require('http');
+const https = require('https');
+const fs = require("fs");
+const read = fs.readFileSync;
+const express = require("express");
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/build/index.html')
-})
+// HTTP server
+let httpApp = express();
+let httpRouter = express.Router();
 
-app.use(express.static(__dirname + '/build'));
+httpApp.use('/', httpRouter);
+httpRouter.get('/', function(req, res){
+    if (req.protocol === "http") {
+      let host = req.get('host');
+      let destination = ['https://', host, req.url].join('');
+      return res.redirect(destination);
+    } else {
+      return res.sendFile(__dirname + '/build/index.html')
+    }
+});
 
-app.listen(80)
+http.createServer(httpApp).listen(80);
+
+httpApp.use(express.static(__dirname + '/build'));
+
+// HTTPS server
+let app = express();
+let certificate = read("./ssl/website.crt", 'utf8');
+let chainLines = read("./ssl/intermediate_domain_ca.crt", 'utf8').split("\n");
+let cert = [];
+let ca = [];
+chainLines.forEach(function(line) {
+  cert.push(line);
+  if (line.match(/-END CERTIFICATE-/)) {
+    ca.push(cert.join("\n"));
+    cert = [];
+  }
+});
+
+let httpsOptions = {
+  key: read('./ssl/privatekey.key'),
+  cert: certificate,
+  ca: ca
+};
+
+secServer = https.createServer(httpsOptions, httpApp);
+secServer.listen(443);
